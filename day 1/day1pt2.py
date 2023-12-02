60aff317540ea37660c1c874805e5d1a28bc7716
Prathu <123509026+SeaM05@users.noreply.github.com>
2023-12-02 01:46:51 +0000

    feat: solved day 1 all parts

--- day 1/day1pt2.py
+++ day 1/day1pt2.py
@@ -16,26 +16,23 @@ def calculate_calibration_sum(calibration_text):
     calibration_sum = 0
     # Split the calibration text into lines
     lines = calibration_text.splitlines()
-
     # Iterate through each line
     for line in lines:
-
-       # print(findnum(line))
         concatenated_number = ""
+        word = ""
         for char in line:
             if char.isdigit():
                 concatenated_number += char
-            elif findnum(line) != -1:
-                concatenated_number += str(findnum(line))
+            else:
+                word += char
+                if findnum(word) != -1:
+                    concatenated_number += str(findnum(word))
         
-            
-        # Extract all numbers from the line and concatenate them
-        #concatenated_number = ''.join(char for char in line if char.isdigit() or (findnum(line)!= -1) )
-        print(concatenated_number)
+        #print(int(concatenated_number[0]) * 10 + int(concatenated_number[-1]))
         # If there is a concatenated number, add the sum of the first and last digits to the calibration sum
-        if concatenated_number:
-            calibration_sum += int(concatenated_number[0]) * 10 + int(concatenated_number[-1])
-           # print(int(concatenated_number[0]) * 10 + int(concatenated_number[-1]))
+      
+        calibration_sum += int(concatenated_number[0]) * 10 + int(concatenated_number[-1])
+        print(int(concatenated_number[0]) * 10 + int(concatenated_number[-1]))
             
     return calibration_sum
 
